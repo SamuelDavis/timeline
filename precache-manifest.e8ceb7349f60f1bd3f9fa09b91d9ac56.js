c4ed266475215fe964561620c5604c61ba145c25
Samuel Davis <samueljakdavis@gmail.com>
2020-10-03 23:43:47 -0400

    Updates

---
+++ precache-manifest.e8ceb7349f60f1bd3f9fa09b91d9ac56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88a76b71f3a8627e619ff6198e645a0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "065a10c5d7028abd735a",
+    "url": "/static/css/main.5aed09cd.chunk.css"
+  },
+  {
+    "revision": "80e2cdd9a45f8d7e1584",
+    "url": "/static/js/2.96297417.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.96297417.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "065a10c5d7028abd735a",
+    "url": "/static/js/main.def2e333.chunk.js"
+  },
+  {
+    "revision": "e2fef5866ba58f0c4200",
+    "url": "/static/js/runtime-main.1649f60d.js"
+  }
+]);
